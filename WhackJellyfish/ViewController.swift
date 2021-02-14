d9fe0799d0f72ad8e12681713fcd8c2f324df3f6
Houssam <houssam.naous1@gmail.com>
2021-02-14 16:29:29 +0100

    added hit detection

--- WhackJellyfish/ViewController.swift
+++ WhackJellyfish/ViewController.swift
@@ -6,14 +6,59 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    @IBAction func play(_ sender: Any) {
+        self.addNode()
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let configuration = ARWorldTrackingConfiguration()
+        
+        self.sceneView.debugOptions = [SCNDebugOptions.showWorldOrigin, SCNDebugOptions.showFeaturePoints]
+        self.sceneView.session.run(configuration)
         // Do any additional setup after loading the view.
+    
+    
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
     }
 
+    func addNode() {
+        let node = SCNNode(geometry: SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0))
+        
+        node.position = SCNVector3(0,0,-1)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        self.sceneView.scene.rootNode.addChildNode(node)
+
+    }
+    
+    
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        let sceneViewTappedOn = sender.view as! SCNView
+        let touchCoordinates = sender.location(in: sceneViewTappedOn)
+        let hitTest = sceneViewTappedOn.hitTest(touchCoordinates)
+        
+        if hitTest.isEmpty {
+            print("nothing")
+        }else{
+            print("Hit something")
+            let result = hitTest.first!
+            let geom = result.node.geometry
+            print(geom)
+            
+        }
+                
+    }
 
 }
 
